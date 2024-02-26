91feaebf153282e68d692d9b416bcaa20b8b401f
Mak0nz <72095988+Mak0nz@users.noreply.github.com>
2024-02-26 11:35:52 +0100

    detect userloginstatus and return widget based on authState

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,12 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:heamed/screens/auth/log_in.dart';
+import 'package:heamed/screens/home/home.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // return either home or authenticate widget
-    return LoginPage();
+    // return either home | loading | signup widget based on authState
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, AsyncSnapshot<User?> snapshot) {
+        if (snapshot.hasData && snapshot.data != null) {
+          return const Home();
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return const LoginPage();
+      },
+    );
   }
 }
