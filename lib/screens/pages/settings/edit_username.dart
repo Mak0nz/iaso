95ab6b6416e850c78d2e28c579bc004ce90523cc
Mak0nz <72095988+Mak0nz@users.noreply.github.com>
2024-05-07 17:34:34 +0200

    change username is functional

--- lib/screens/pages/settings/edit_username.dart
+++ lib/screens/pages/settings/edit_username.dart
@@ -1,5 +1,13 @@
+// ignore_for_file: unused_field, prefer_final_fields, prefer_typing_uninitialized_variables
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:iaso/services/firebase_auth.dart';
+import 'package:iaso/widgets/animated_button_widget.dart';
+import 'package:iaso/widgets/form_container_widget.dart';
+import 'package:iaso/widgets/toast.dart';
 import 'package:wolt_modal_sheet/wolt_modal_sheet.dart';
 
 class EditUsernameModal extends StatefulWidget {
@@ -10,6 +18,35 @@ class EditUsernameModal extends StatefulWidget {
 }
 
 class _EditUsernameModalState extends State<EditUsernameModal> {
+  bool _isSaving = false;
+
+  final FirebaseAuthService _auth = FirebaseAuthService();
+
+  TextEditingController _usernameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchExistingUsername();
+  }
+
+  Future fetchExistingUsername() async {
+    final user = FirebaseAuth.instance.currentUser;
+    final docRef = FirebaseFirestore.instance
+        .collection('users')
+        .doc(user?.email);
+        
+    final docSnapshot = await docRef.get();
+    if (docSnapshot.exists) {
+      final data = docSnapshot.data();
+      // Check for non-zero fields and update controllers
+      if (data!['username'] != 0) {
+        _usernameController.text = data['username'].toString();
+      }
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -20,7 +57,32 @@ class _EditUsernameModalState extends State<EditUsernameModal> {
               padding: const EdgeInsets.fromLTRB(10, 10, 10, 35),
               child: Column(
                 children: [
+                  Text("Felhasználónév módosítása:", style: TextStyle(
+                    fontWeight: FontWeight.bold, 
+                    fontSize: 24,
+                  ),),
                   
+                  SizedBox(height: 5,),
+
+                  FormContainerWidget(
+                  controller: _usernameController,
+                  hintText: "",
+                  isPasswordField: false,
+                  ),
+
+                  SizedBox(height: 15,),
+
+                  AnimatedButton(
+                    onTap: () {
+                      final username = Username(
+                        username: _usernameController.text
+                      );
+                      saveUsername(username);
+                    }, 
+                    text: "Mentés", 
+                    progressEvent: _isSaving,
+                  ),
+
                 ],
               ),
             ),
@@ -33,4 +95,36 @@ class _EditUsernameModalState extends State<EditUsernameModal> {
       child: Icon(FontAwesomeIcons.penToSquare),
     );
   }
+
+  Future saveUsername(Username username) async {
+    setState(() {
+      _isSaving = true;  
+    });
+
+    final user = FirebaseAuth.instance.currentUser;
+    final docRef = FirebaseFirestore.instance.collection('users').doc(user?.email);
+    final json = username.toJson();
+    await docRef.update(json);
+    
+    setState(() {
+      _isSaving = false;  
+    });
+
+    showToast(message: "Elmentve");
+    // ignore: use_build_context_synchronously
+    Navigator.pop(context);
+  }
+
+}
+
+class Username {
+  var username;
+
+  Username({
+    this.username
+  });
+
+  Map<String, dynamic> toJson() => {
+    'username': username,
+  };
 }
